7173d5ce684c9931bdd3f08cbd601534c8c53a39
KostyaNike <deras1234oo@gmail.com>
2025-05-23 16:52:43 +0200

    add ads.txt

--- solar_panel/urls.py
+++ solar_panel/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('solar_main.urls')),
     path('', include('solar_main.urls')),
+    re_path(r'^ads\.txt$', serve, {
+        'path': os.path.join(settings.BASE_DIR, 'static/ads.txt'),
+        'document_root': '/',
+    }),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
